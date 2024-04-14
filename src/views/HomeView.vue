eb0979f6bb452223fcdf2c62641cd40b169be5d8
DEATHhappy <deathhappy.z@gmail.com>
2024-04-14 21:53:22 +0900

    Add record

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,56 +1,122 @@
 <template>
     <div>
-        <div class="title">Total: {{ disabledCount }} / 29</div>
+        <div class="title" style="flex-direction: column;">
+            <div class="title">
+                <div class="title_text">總數: 29 已收集：{{ disabledCount }} 未收集：{{29 - disabledCount}}</div>
+                <el-button type="primary" @click="resetDisabled">清除紀錄</el-button>
+            </div>            
+            <div class="title">
+                <div class="title_text">單排數量: </div>
+                <el-select v-model="rowNum" placeholder="Select" style="width: 240px">
+                    <el-option
+                        v-for="item in 29"
+                        :key="item"
+                        :label="item"
+                        :value="item"
+                    />
+                </el-select>
+            </div>            
+        </div>        
         <div class="image-row">
             <imageCard v-for="(img, i) in images" :src="img.name" :disabled="img.disabled" 
-                :key="i" @click="img.disabled=!img.disabled" />
-            
+                :key="i" @click="clickCard(img, i)" :style="dynamicWidth" />
         </div>
     </div>
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue';
+import { ref, computed, onMounted, watch } from 'vue';
 import imageCard from '@/components/imageCard.vue'
 const images = ref([]);
+const rowNum = ref(8);
+const disabledArray = ref([])
+
+onMounted(() => {
+    loadImages()
+})
 
 const loadImages = () => {
     images.value = []
     const requireImages = import.meta.glob('../assets/hsr1st/*.png');
-    console.log(requireImages);
+    //console.log(requireImages);
+    disabledArray.value = getArrayFromCookie('disabledArray')
     for (const key in requireImages) {
         // console.log(key);
         const match = key.match(/([^\/]+)\.\w+$/);
 
         const partyName = match ? match[1] : null;
         //console.log('Party Name:', partyName);
-
-        images.value.push({name: partyName, path: key, disabled: false})
-
+        let isDisabled = disabledArray.value.includes(partyName)
+        images.value.push({name: partyName, path: key, disabled: isDisabled})
     }
-    console.log(images.value)
+
+    
+    for (let index = 0; index < images.value.length; index++) {
+        const element = images.value[index];        
+    }
+    //console.log(images.value)
 };
 
-// Split images into chunks of 6
-const imageChunks = computed(() => {
-    const chunkSize = 6;
-    const chunks = [];
-    for (let i = 0; i < images.value.length; i += chunkSize) {
-        chunks.push(images.value.slice(i, i + chunkSize));
-    }
-    return chunks;
+const dynamicWidth = computed(() => {
+    return `width: calc(100% / ${rowNum.value})`;
 });
 
 const disabledCount = computed(() => {
     return 29 - images.value.filter(element => element.disabled).length
 })
 
-onMounted(loadImages);
+function clickCard(img, index) {
+    img.disabled=!img.disabled
+    setArrayToCookie('disabledArray', images.value.filter(item => item.disabled === true).map(item => item.name))
+}
+
+function resetDisabled() {
+    setArrayToCookie('disabledArray', [])
+    loadImages()
+}
+
+function getCookie(name) {
+  const cookieString = document.cookie;
+  const cookies = cookieString.split('; ');
+
+  for (const cookie of cookies) {
+    const [cookieName, cookieValue] = cookie.split('=');
+    if (cookieName === name) {
+      return decodeURIComponent(cookieValue);
+    }
+  }
+
+  return null;
+}
+
+function setCookie(name, value, daysToExpire=365) {
+  const expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + daysToExpire);
+
+  const cookieString = `${encodeURIComponent(name)}=${encodeURIComponent(value)}; expires=${expirationDate.toUTCString()}; path=/`;
+  document.cookie = cookieString;
+}
+
+function setArrayToCookie(name, array, daysToExpire) {
+  const arrayString = JSON.stringify(array);
+  setCookie(name, arrayString, daysToExpire);
+}
+
+function getArrayFromCookie(name) {
+  const arrayString = getCookie(name);
+  return arrayString ? JSON.parse(arrayString) : [];
+}
+
 
 </script>
 
 <style scoped>
 .title {
+    display: flex;
+    gap: 8px;
+}
+
+.title_text {
     font-size: 24px;
 }
 
